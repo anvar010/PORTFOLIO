d6672bdafd58af555e6ae6f6fa51b66503dbd689
Anvarsha <anvarshaknavas588@gmail.com>
2026-02-13 19:24:12 +0400

    Update vite config to use conditional base path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [react()],
-    base: '/PORTFOLIO/',
+    base: command === 'build' ? '/PORTFOLIO/' : '/',
     server: {
         port: 5173,
         open: true
     }
-})
+}))
